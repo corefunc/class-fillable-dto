98aa69bc0744ea66fec1980a435bdf7b9dd41cef
Anton Trofimenko <r37r0m0d3l@protonmail.com>
2021-08-09 23:08:42 +0300

    feat: update formatting to match map file

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const class_validator_1 = require("class-validator");
 const safe_1 = require("@corefunc/corefunc/json/stringify/safe");
 const capitalize_1 = require("@corefunc/corefunc/text/case/capitalize");
 const OPTIONS_DEFAULT = {
-    class: false,
-    prettify: true,
-    property: false,
-    value: false,
+  class: false,
+  prettify: true,
+  property: false,
+  value: false,
 };
 /**
  * @summary
@@ -26,181 +26,188 @@ const OPTIONS_DEFAULT = {
  * ```
  */
 class FillableDto {
-    static fromJSON(json) {
-        return this.fromPlain(JSON.parse(json));
+  static fromJSON(json) {
+    return this.fromPlain(JSON.parse(json));
+  }
+  static fromPlain(plain) {
+    // @ts-ignore
+    return class_transformer_1.plainToClass(this, plain);
+  }
+  /**
+   * @constructor
+   * @param {Object=} attributes Data Transfer Object attributes to be set
+   * @param {Array.<String>=} includeKeys Include only specific properties
+   * @protected
+   */
+  constructor(attributes, includeKeys) {
+    this.assign(attributes, includeKeys);
+  }
+  toJSON() {
+    return this.toObject();
+  }
+  toObject() {
+    return class_transformer_1.classToPlain(
+      clone_marshalling_1.cloneMarshalling(this)
+    );
+  }
+  toString() {
+    return safe_1.jsonStringifySafe(this.toObject());
+  }
+  isValid(silent = false) {
+    const validationErrors = class_validator_1.validateSync(this);
+    if (silent) {
+      return Boolean(validationErrors.length === 0);
     }
-    static fromPlain(plain) {
+    if (validationErrors.length === 0) {
+      return true;
+    }
+    const constructorName = this.constructor.name;
+    const errorText = validationErrors
+      .map(function errorToSentence(error) {
+        const constraints = {};
+        if ("constraints" in error) {
+          Object.assign(constraints, error.constraints);
+        } else if ("children" in error) {
+          return (error.children || []).map(errorToSentence).join(" ");
+        }
+        const failed = `${Object.values(constraints)
+          .map((text) => `${capitalize_1.textCaseCapitalize(String(text))}`)
+          .join(". ")}`;
+        const where = `Error in [${constructorName}].`;
+        const property = `Property [${error.property}].`;
+        const value = `Value is [${safe_1.jsonStringifySafe(error.value)}].`;
+        const message = `Failed: ${failed}.`;
+        return `${where} ${property} ${value} ${message}`;
+      })
+      .join(" ");
+    throw new Error(errorText);
+  }
+  assign(attributes, includeKeys) {
+    if (!attributes) {
+      return this;
+    }
+    let keys;
+    if (includeKeys) {
+      keys = includeKeys;
+    } else {
+      keys = Object.keys(attributes);
+    }
+    keys.forEach((key) => {
+      if (key in attributes) {
+        // Trigger key setter for object instance
         // @ts-ignore
-        return class_transformer_1.plainToClass(this, plain);
+        this[key] = clone_marshalling_1.cloneMarshalling(attributes[key]);
+      }
+    });
+    return this;
+  }
+  assignDefault(
+    key,
+    value,
+    skipIfKeyNotInObject = false,
+    setOnlyIfUndefined = true
+  ) {
+    const isKeyInObject = key in this;
+    if (skipIfKeyNotInObject && !isKeyInObject) {
+      return this;
     }
-    /**
-     * @constructor
-     * @param {Object=} attributes Data Transfer Object attributes to be set
-     * @param {Array.<String>=} includeKeys Include only specific properties
-     * @protected
-     */
-    constructor(attributes, includeKeys) {
-        this.assign(attributes, includeKeys);
+    // @ts-ignore
+    if (setOnlyIfUndefined && this[key] !== undefined) {
+      return this;
     }
-    toJSON() {
-        return this.toObject();
+    // @ts-ignore
+    this[key] = clone_marshalling_1.cloneMarshalling(value);
+    return this;
+  }
+  getError(options) {
+    const errors = this.getErrors(options);
+    if (errors.length === 0) {
+      return null;
     }
-    toObject() {
-        return class_transformer_1.classToPlain(clone_marshalling_1.cloneMarshalling(this));
+    return errors.join(" ").trim();
+  }
+  getErrors(options) {
+    const opts = this.buildOptions(options);
+    const validationErrors = class_validator_1.validateSync(this, {
+      validationError: { target: false },
+    });
+    if (validationErrors.length === 0) {
+      return [];
     }
-    toString() {
-        return safe_1.jsonStringifySafe(this.toObject());
+    const constructorName = this.constructor.name;
+    return validationErrors.map(function errorToSentence(error) {
+      const constraints = {};
+      if ("constraints" in error) {
+        Object.assign(constraints, error.constraints);
+      } else if ("children" in error) {
+        return (error.children || []).map(errorToSentence).join(" ");
+      }
+      let where = "";
+      if (opts.class) {
+        where = `Error in [${constructorName}].`;
+      }
+      let property = "";
+      if (opts.property) {
+        property = `Property [${error.property}].`;
+      }
+      let value = "";
+      if (opts.value) {
+        value = `Value is [${safe_1.jsonStringifySafe(error.value)}].`;
+      }
+      let message;
+      if (opts.prettify) {
+        const failedPretty = Object.values(constraints)
+          .map((text) =>
+            String(text).replace(error.property, `[${error.property}]`).trim()
+          )
+          .map((text) => capitalize_1.textCaseCapitalize(String(text)))
+          .map((text) => String(text).trim())
+          .join(". ");
+        message = `${failedPretty}.`;
+      } else {
+        message = Object.values(constraints)
+          .map((text) => String(text).trim())
+          .join(". ");
+      }
+      return `${where} ${property} ${value} ${message}`.trim();
+    });
+  }
+  buildOptions(options) {
+    if (!options) {
+      return OPTIONS_DEFAULT;
     }
-    isValid(silent = false) {
-        const validationErrors = class_validator_1.validateSync(this);
-        if (silent) {
-            return Boolean(validationErrors.length === 0);
-        }
-        if (validationErrors.length === 0) {
-            return true;
-        }
-        const constructorName = this.constructor.name;
-        const errorText = validationErrors
-            .map(function errorToSentence(error) {
-            const constraints = {};
-            if ("constraints" in error) {
-                Object.assign(constraints, error.constraints);
-            }
-            else if ("children" in error) {
-                return (error.children || []).map(errorToSentence).join(" ");
-            }
-            const failed = `${Object.values(constraints)
-                .map((text) => `${capitalize_1.textCaseCapitalize(String(text))}`)
-                .join(". ")}`;
-            const where = `Error in [${constructorName}].`;
-            const property = `Property [${error.property}].`;
-            const value = `Value is [${safe_1.jsonStringifySafe(error.value)}].`;
-            const message = `Failed: ${failed}.`;
-            return `${where} ${property} ${value} ${message}`;
-        })
-            .join(" ");
-        throw new Error(errorText);
+    if (!class_validator_1.isObject(options)) {
+      return OPTIONS_DEFAULT;
     }
-    assign(attributes, includeKeys) {
-        if (!attributes) {
-            return this;
-        }
-        let keys;
-        if (includeKeys) {
-            keys = includeKeys;
-        }
-        else {
-            keys = Object.keys(attributes);
-        }
-        keys.forEach((key) => {
-            if (key in attributes) {
-                // Trigger key setter for object instance
-                // @ts-ignore
-                this[key] = clone_marshalling_1.cloneMarshalling(attributes[key]);
-            }
-        });
-        return this;
-    }
-    assignDefault(key, value, skipIfKeyNotInObject = false, setOnlyIfUndefined = true) {
-        const isKeyInObject = key in this;
-        if (skipIfKeyNotInObject && !isKeyInObject) {
-            return this;
-        }
+    return {
+      class:
         // @ts-ignore
-        if (setOnlyIfUndefined && this[key] !== undefined) {
-            return this;
-        }
+        "class" in options && class_validator_1.isBoolean(options["class"])
+          ? // @ts-ignore
+            options["class"]
+          : OPTIONS_DEFAULT.class,
+      prettify:
         // @ts-ignore
-        this[key] = clone_marshalling_1.cloneMarshalling(value);
-        return this;
-    }
-    getError(options) {
-        const errors = this.getErrors(options);
-        if (errors.length === 0) {
-            return null;
-        }
-        return errors.join(" ").trim();
-    }
-    getErrors(options) {
-        const opts = this.buildOptions(options);
-        const validationErrors = class_validator_1.validateSync(this, {
-            validationError: { target: false },
-        });
-        if (validationErrors.length === 0) {
-            return [];
-        }
-        const constructorName = this.constructor.name;
-        return validationErrors.map(function errorToSentence(error) {
-            const constraints = {};
-            if ("constraints" in error) {
-                Object.assign(constraints, error.constraints);
-            }
-            else if ("children" in error) {
-                return (error.children || []).map(errorToSentence).join(" ");
-            }
-            let where = "";
-            if (opts.class) {
-                where = `Error in [${constructorName}].`;
-            }
-            let property = "";
-            if (opts.property) {
-                property = `Property [${error.property}].`;
-            }
-            let value = "";
-            if (opts.value) {
-                value = `Value is [${safe_1.jsonStringifySafe(error.value)}].`;
-            }
-            let message;
-            if (opts.prettify) {
-                const failedPretty = Object.values(constraints)
-                    .map((text) => String(text).replace(error.property, `[${error.property}]`).trim())
-                    .map((text) => capitalize_1.textCaseCapitalize(String(text)))
-                    .map((text) => String(text).trim())
-                    .join(". ");
-                message = `${failedPretty}.`;
-            }
-            else {
-                message = Object.values(constraints)
-                    .map((text) => String(text).trim())
-                    .join(". ");
-            }
-            return `${where} ${property} ${value} ${message}`.trim();
-        });
-    }
-    buildOptions(options) {
-        if (!options) {
-            return OPTIONS_DEFAULT;
-        }
-        if (!class_validator_1.isObject(options)) {
-            return OPTIONS_DEFAULT;
-        }
-        return {
-            class: 
-            // @ts-ignore
-            "class" in options && class_validator_1.isBoolean(options["class"])
-                ? // @ts-ignore
-                    options["class"]
-                : OPTIONS_DEFAULT.class,
-            prettify: 
-            // @ts-ignore
-            "prettify" in options && class_validator_1.isBoolean(options["prettify"])
-                ? // @ts-ignore
-                    options["prettify"]
-                : OPTIONS_DEFAULT.prettify,
-            property: 
-            // @ts-ignore
-            "property" in options && class_validator_1.isBoolean(options["property"])
-                ? // @ts-ignore
-                    options["property"]
-                : OPTIONS_DEFAULT.property,
-            value: 
-            // @ts-ignore
-            "value" in options && class_validator_1.isBoolean(options["value"])
-                ? // @ts-ignore
-                    options["value"]
-                : OPTIONS_DEFAULT.value,
-        };
-    }
+        "prettify" in options &&
+        class_validator_1.isBoolean(options["prettify"])
+          ? // @ts-ignore
+            options["prettify"]
+          : OPTIONS_DEFAULT.prettify,
+      property:
+        // @ts-ignore
+        "property" in options &&
+        class_validator_1.isBoolean(options["property"])
+          ? // @ts-ignore
+            options["property"]
+          : OPTIONS_DEFAULT.property,
+      value:
+        // @ts-ignore
+        "value" in options && class_validator_1.isBoolean(options["value"])
+          ? // @ts-ignore
+            options["value"]
+          : OPTIONS_DEFAULT.value,
+    };
+  }
 }
 exports.FillableDto = FillableDto;
 //# sourceMappingURL=index.js.map
